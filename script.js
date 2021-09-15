01a25d1ca1631ac6722109f15be9798a5194dc71
linhdoan9513 <78831085+linhdoan9513@users.noreply.github.com>
2021-09-15 17:21:27 -0400

    "reOrganize"

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Navigation
 
-let hamburger = document.querySelector(".hamburger");
-let menu = document.querySelector(".menuList");
+const myApp = {};
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menuList");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
@@ -11,49 +12,40 @@ hamburger.addEventListener("click", function () {
 //Image Carousel
 //listen
 
-// const imageClassName = ;
 const images = document.querySelectorAll(".carouselImage");
 let currentIndex = 0;
 const totalImages = images.length;
 
-function setInitialClass() {
+myApp.setInitialClass = function () {
   images[currentIndex].classList.add("active");
   images[currentIndex + 1].classList.add("active");
   images[currentIndex + 2].classList.add("active");
   console.log(currentIndex, currentIndex + 1, currentIndex + 2);
-}
+};
 
-function setEventListeners() {
+myApp.setEventListeners = function () {
   const buttonLeft = document.querySelector(".carouselLeft");
   const buttonRight = document.querySelector(".carouselRight");
-  buttonLeft.addEventListener("click", moveButton);
-  buttonRight.addEventListener("click", moveButton);
-}
+  buttonLeft.addEventListener("click", myApp.moveButton);
+  buttonRight.addEventListener("click", myApp.moveButton);
+};
 
-function moveButton() {
+myApp.moveButton = function () {
   images[currentIndex].classList.remove("active");
   if (currentIndex + 2 >= totalImages - 1) {
     images[totalImages - 1].classList.remove("active");
     images[totalImages - 2].classList.remove("active");
     currentIndex = 0;
-    setInitialClass();
+    myApp.setInitialClass();
   } else {
     currentIndex++;
-    setInitialClass();
+    myApp.setInitialClass();
   }
-}
+};
 
-// function mod(n, m) {
-//   return ((n % m) + m) % m;
-// }
+myApp.init = function () {
+  myApp.setInitialClass();
+  myApp.setEventListeners();
+};
 
-setInitialClass();
-setEventListeners();
-
-// currentIndex = (currentIndex + 1) % 5;
-//given an index, show 3 images starting from an index:
-//- (index +1) % 5 and (index +2) % 5
-
-//click arrow left or right, have to change the index
-//- click right: index = (index + 1)% 5, if index == 4; (index +1) % 5 = 0
-//- click left: index = (index - 1) % 5, if index == 0; (0-1) % 5 = 4;
+myApp.init();
