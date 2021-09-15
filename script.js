cca4f1d8bb92a0e64376f4197ffb17d0b41115e5
linhdoan9513 <78831085+linhdoan9513@users.noreply.github.com>
2021-09-15 14:48:26 -0400

    "updated"

--- script.js
+++ script.js
@@ -11,46 +11,45 @@ hamburger.addEventListener("click", function () {
 //Image Carousel
 //listen
 
-const imageClassName = ".carouselImage";
-const images = document.querySelectorAll(imageClassName);
-
+// const imageClassName = ;
+const images = document.querySelectorAll(".carouselImage");
 let currentIndex = 0;
-
 const totalImages = images.length;
 
 function setInitialClass() {
-  console.log(images, currentIndex);
   images[currentIndex].classList.add("active");
   images[currentIndex + 1].classList.add("active");
   images[currentIndex + 2].classList.add("active");
+  console.log(currentIndex, currentIndex + 1, currentIndex + 2);
 }
-
-// function setFollowingImages() {}
 
 function setEventListeners() {
   const buttonLeft = document.querySelector(".carouselLeft");
   const buttonRight = document.querySelector(".carouselRight");
-  buttonLeft.addEventListener("click", moveLeft);
-  buttonRight.addEventListener("click", moveRight);
+  buttonLeft.addEventListener("click", moveButton);
+  buttonRight.addEventListener("click", moveButton);
 }
 
-function moveLeft() {
-  currentIndex = mod(currentIndex - 1, 5);
-  setInitialClass();
+function moveButton() {
+  images[currentIndex].classList.remove("active");
+  if (currentIndex + 2 >= totalImages - 1) {
+    images[totalImages - 1].classList.remove("active");
+    images[totalImages - 2].classList.remove("active");
+    currentIndex = 0;
+    setInitialClass();
+  } else {
+    currentIndex++;
+    setInitialClass();
+  }
 }
 
-function moveRight() {
-  currentIndex = mod(currentIndex + 1, 5);
-  setInitialClass();
-}
-
-function mod(n, m) {
-  return ((n % m) + m) % m;
-}
+// function mod(n, m) {
+//   return ((n % m) + m) % m;
+// }
 
 setInitialClass();
 setEventListeners();
-
+// currentIndex = (currentIndex + 1) % 5;
 //given an index, show 3 images starting from an index:
 //- (index +1) % 5 and (index +2) % 5
 
